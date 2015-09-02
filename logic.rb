b251d959db927bbd6cc71ddb559c86bd92825f59
cpintoval <cpintovalderrama@gmail.com>
2015-09-02 21:21:14 +0000

    Initial commit

---
+++ logic.rb
@@ -0,0 +1,72 @@
+# This file contains the actual logic of the Math Game
+require './player'
+require './invalid_guess_error'
+
+# Initializing player's lives
+@players = [ Player.new("Player 1", 1), Player.new("Player 2", 2)]
+@actual_question = ""
+@actual_answer = nil
+@actual_player = 1
+
+def generate_question
+  operation = Random.rand(1..4)
+  num1 = Random.rand(1..20)
+  num2 = Random.rand(1..20)
+  case operation
+  when 1
+    # Addition
+    @actual_answer = num1 + num2
+    "What does #{num1} plus #{num2} equal?"
+  when 2
+    # Substraction
+    @actual_answer = num1 - num2
+    "What does #{num1} minus #{num2} equal?"
+  when 3
+    # Multiplication
+    @actual_answer = num1 * num2
+    "What does #{num1} times #{num2} equal?"
+  when 4
+    # Division
+    @actual_answer = num1 / num2
+    "What does #{num1} divided by #{num2} equal (rounded to the nearest integer)?"
+  end
+end
+
+def verify_answer(answer)
+
+  if !(/\A[-+]?\d+\z/ === answer)
+    raise InvalidGuessError, "Guess is not an Integer"
+  end
+  
+  answer = answer.to_i
+  output = @actual_answer == answer
+  if output
+    @players[@actual_player - 1].gain_point
+  else
+    @players[@actual_player - 1].lose_life
+  end
+  output
+end
+
+def next_turn
+  if @actual_player == 1
+    @actual_player = 2
+  else
+    @actual_player = 1
+  end
+end
+
+def get_loser
+  if @players[0].lives == 0
+    @players[0].name
+  elsif @players[1].lives == 0
+    @players[1].name
+  else
+    "No loser yet"
+  end  
+end
+
+def restart_lives
+  @players[0].lives = 3
+  @players[1].lives = 3
+end
---
+++ main.rb
@@ -0,0 +1,58 @@
+# This is the main entrypoint into the program
+# It requires the other files/gems that it needs
+
+require 'colorize'
+require './logic'
+require './player'
+require './invalid_guess_error'
+
+def play
+  puts "*************************************************************************".yellow
+  puts "                        LET'S GET READY TO RUMBLE!                       ".yellow
+  while(@players[0].lives > 0 && @players[1].lives > 0)
+    puts "- #{@players[@actual_player - 1].name}: #{generate_question}"
+    while true
+      begin
+        print "Answer: "
+        answer = gets.chomp
+        puts verify_answer(answer) ? "- That's correct! You got 1 point!".green : "- Too bad, right answer is #{@actual_answer}. Lost 1 life.".red
+        break
+      rescue Exception => e
+        puts "#{e.class}: #{e.message} - Try again".red
+      end
+    end
+    puts "-------------------------------------------------------------------------".yellow
+    next_turn
+  end
+  # Game ended. Display who won and scores
+  puts "                           Round Finished                                ".yellow
+  puts "- #{get_loser} lost all his lives!"
+  puts "~~~ Scores ~~~".yellow
+  puts "- #{@players[0].name}: #{@players[0].score} points."
+  puts "- #{@players[1].name}: #{@players[1].score} points."
+end
+
+puts "************************  Multiplayer Math Game  ************************".yellow
+puts "- Let's start by selecting your names..."
+puts "- Player 1 enter your name: "
+p1_name = gets.chomp
+@players[0].name = p1_name != "" ? p1_name : "Player 1"
+puts "- Welcome to the game #{@players[0].name}!"
+puts "- Player 2 enter your name: "
+p2_name = gets.chomp
+@players[1].name = p2_name != "" ? p2_name : "Player 2"
+puts "- Welcome to the game #{@players[1].name}!"
+while true
+  play
+  puts "- Wanna play again? (yes/no)".yellow
+  cmd = gets.chomp
+  if cmd.downcase == 'no'
+    puts "- No problem! Come back later for more Math Fun! Bye! :)".yellow
+    break
+  elsif cmd.downcase == 'yes'
+    restart_lives
+  else
+    puts "- Oh, oh...you broke the game, dude!".red
+    break
+  end
+end
